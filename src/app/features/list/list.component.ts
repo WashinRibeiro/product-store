155bd498feea4f92fefcb621525f1937f81867f2
Washington Ribeiro <washinuchiha@gmail.com>
2024-05-01 11:32:34 -0300

    feat: adding a list of products

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
+  products: any[] = []
+
+  httpClient = inject(HttpClient)
+
+  ngOnInit() {
+    this.httpClient.get('/api/products').subscribe({
+      next: (products: any) => {
+        this.products = products
+      }
+    })
+  }
 }
