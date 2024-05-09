b998226fbce5f1cce7d41f2e7a6a03df46866b85
Washington Ribeiro <washinuchiha@gmail.com>
2024-05-08 23:46:04 -0300

    feat: add button and link to create product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { IProduct } from '../../shared/interfaces/product.inferface';
 import { ProductsService } from './../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
