c830163ef989c7d11bd17ac632aaafc7e2530d80
Washington Ribeiro <washinuchiha@gmail.com>
2024-05-09 01:52:08 -0300

    feat: adding a feedback message when creating a product

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,19 +1,33 @@
 import { ProductsService } from './../../shared/services/products.service';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from "@angular/material/form-field"
-import { MatInputModule } from "@angular/material/input"
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
 
-  productsService = inject(ProductsService)
+  productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
   form = new FormGroup({
     title: new FormControl<string>('', {
@@ -23,10 +37,18 @@ export class CreateComponent {
   });
 
   onSubmit() {
-    this.productsService.post({title: this.form.controls.title.value}).subscribe(() => {
-      this.form.reset();
-      alert('Product created');
-    });
+    this.productsService
+      .post({ title: this.form.controls.title.value })
+      .subscribe(() => {
+        this.matSnackBar.open('Produto criado com sucesso', 'Ok', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        })
 
+        this.router.navigateByUrl('/');
+      });
+
+    
   }
 }
